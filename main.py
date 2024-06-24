93c10f342bb3e2bd3908719c15ede2bf7bfabdf0
TH <00001116754629sp@al.educacao.sp.gov.br>
2024-06-24 12:24:51 -0300

    começo do projeto

---
+++ main.py
@@ -0,0 +1,10 @@
+print('########################################')
+print('#         ALGORITMOS EM PYTHON         #')
+print('########################################')
+print('# Escolha a opção desejada             #')
+print('#                                      #')
+print('# 1. Fibonacci                         #')
+print('########################################')
+      #')
+
+
