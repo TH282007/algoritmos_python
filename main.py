20eb1f514bf1915b4bb368d43078068064ac4ce7
TH <00001116754629sp@al.educacao.sp.gov.br>
2024-06-26 12:20:18 -0300

    aula 2

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ print('#                                      #')
 print('# 1. Fibonacci                         #')
 print('########################################')
       #')
+try:
+     choice = int(input())
 
-
+     if choice == 1: 
+         print('FIB')
+except:
+    print ('ocorreu um erro...')
---
+++ scripts/fibonacci.py
@@ -0,0 +1,7 @@
+
+
+def fibonacci(qtt:int):
+    n1 = 0
+    n2 = 1
+    sequence = [0,1]
+    return sequence 
